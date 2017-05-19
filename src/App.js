2c8f51828e0663ee9f151e913f617299f5ac25e1
autopp <autopp.inc@gmail.com>
2017-05-19 23:00:32 +0900

    port top header to App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <h1>MHXX Atack Comparator</h1>
+          </div>
         </div>
-        <Child prefix="For get started" />
       </div>
-    );
-  }
-}
-
-
-let filepath = "src/App.js";
-
-class Child extends Component {
-  render() {
-    return (
-      <p className="App-intro">
-        {this.props.prefix}, edit <code>{filepath}</code> and save to reload.
-      </p>
     );
   }
 }
