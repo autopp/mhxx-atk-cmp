06bea66d6bab3cadd67c7ce1e430ffe0a4e3f90f
autopp <autopp.inc@gmail.com>
2017-05-22 19:01:42 +0900

    delegate item to input class

--- src/InputRow.js
+++ src/InputRow.js
@@ -3,19 +3,20 @@ import React, { Component } from 'react';
 class InputRow extends Component {
   render() {
     let inputClass = this.getInputClass();
+    let item = this.props.item;
     return (
       <div className="row inputRow">
         <div className={InputRow.labelColClass}>
           <p>{this.props.labelText}</p>
         </div>
         <div className={InputRow.inputColClass}>
-          {React.createElement(inputClass, Object.assign({ pos: 'right' }, this.props.inputProps))}
+          {React.createElement(inputClass, Object.assign({ pos: 'left', item: item, value: this.props.state.left[item] }, this.props.inputProps))}
         </div>
         <div className={InputRow.syncColClass}>
-          <SyncInputs item={this.props.item} synced={this.props.state.sync[this.props.item]} onChange={this.props.setSync} />
+          <SyncInputs item={item} synced={this.props.state.sync[item]} onChange={this.props.setSync} />
         </div>
         <div className={InputRow.inputColClass}>
-          {React.createElement(inputClass, Object.assign({ pos: 'right' }, this.props.inputProps))}
+          {React.createElement(inputClass, Object.assign({ pos: 'right', item: item, value: this.props.state.right[item] }, this.props.inputProps))}
         </div>
       </div>
     );
