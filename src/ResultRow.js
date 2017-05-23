1af3d477f466115108c77504d815f1099871b2f3
autopp <autopp.inc@gmail.com>
2017-05-24 08:31:49 +0900

    port ResultRow

---
+++ src/ResultRow.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { labelColClass, inputColClass, syncColClass } from './InputRow';
+
+class ResultRow extends Component {
+  render() {
+    return (
+      <div className="row resultRow">
+        <div className={labelColClass}>
+          <p>計算結果</p>
+        </div>
+        <div className={inputColClass}>
+          <Result result={this.props.leftResult} />
+        </div>
+        <div className={syncColClass}>
+          <button type="button" className="btn btn-success btn-xs" onClick={this.props.onClick}>Sync all</button>
+        </div>
+        <div className={inputColClass}>
+          <Result result={this.props.rightResult} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Result;
