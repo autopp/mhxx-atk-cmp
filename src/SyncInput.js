adaf08712837c72dba09a29553bc1b6723073d2d
autopp <autopp.inc@gmail.com>
2017-05-27 00:06:27 +0900

    fix text of sync parts

--- src/SyncInput.js
+++ src/SyncInput.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SyncInput extends Component {
   onClick = e => this.props.onChange(this.props.item, !this.props.synced)
   render() {
-    let [style, text] = this.props.synced ? ['btn-info', 'btn-default'] : ['Synced', 'Unsynced'];
+    let [style, text] = this.props.synced ? ['btn-info', 'Synced'] : ['btn-default', 'Unsynced'];
     let className = `btn ${style} btn-xs sync-button`;
     return (
       <button type="button" className={className} onClick={this.onClick}>{text}</button>
