3dceb20cea58061472ee9ba123ee5777150bd059
autopp <autopp.inc@gmail.com>
2017-05-26 23:47:12 +0900

    fix import from InputRow

--- src/ResultRow.js
+++ src/ResultRow.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import Result from './Result';
-import { labelColClass, inputColClass, syncColClass } from './InputRow';
+import InputRow from './InputRow';
 
 class ResultRow extends Component {
   render() {
     return (
       <div className="row resultRow">
-        <div className={labelColClass}>
+        <div className={InputRow.labelColClass}>
           <p>計算結果</p>
         </div>
-        <div className={inputColClass}>
+        <div className={InputRow.inputColClass}>
           <Result result={this.props.leftResult} />
         </div>
-        <div className={syncColClass}>
+        <div className={InputRow.syncColClass}>
           <button type="button" className="btn btn-success btn-xs" onClick={this.props.onClick}>Sync all</button>
         </div>
-        <div className={inputColClass}>
+        <div className={InputRow.inputColClass}>
           <Result result={this.props.rightResult} />
         </div>
       </div>
