5190e1b2563060a574edbc4eafe0786cb3d5cb3b
autopp <autopp.inc@gmail.com>
2017-05-29 08:42:57 +0900

    fix style of SectionRow

--- src/SectionRow.js
+++ src/SectionRow.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SectionRow extends Component {
   render() {
     return (
-      <div>
+      <div className="row">
         <div className="col-lg-6"><h3>{this.props.text}</h3></div>
       </div>
     );
